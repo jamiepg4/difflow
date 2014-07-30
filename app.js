7b9b34d71de0f9087acd0c3fbd65ffe86498c11a
Zach Hawtof <zhawtof@gmail.com>
2014-07-30 14:08:03 -0700

    Sauce Labs retrieves image for screenshot

--- app.js
+++ app.js
@@ -25,133 +25,58 @@ mongoose.connect('mongodb://localhost/data/db')
 mongoose.connection.on('open', function(){
 
 
-
-
-var Schema = mongoose.Schema;
-var ObjectId = mongoose.Types.ObjectId;
-
-var screenshotSchema = new Schema({
-    screenshotName: String,
-    screenshotImage: {
-        image: Buffer,
-        encoding: String,
-        contentType: String
-    }
-})
-var Screenshot = mongoose.model('Screenshot', screenshotSchema);
-
-
-var screenshotTimelineSchema = new Schema({
-    testName: {
-        type: String,
-    },
-    screenshotName: {
-        type: String,
-    },
-    screenshots: {
-        type: [Schema.ObjectId],
-    },
-    baselineScreenshot: {
-        type: Schema.ObjectId,
-    },
-    baselineLastChanged: {
-        type: Date,
-        default: Date.now
-    },
-    os: String,
-    browser: {
-        type: String,
-        enum: ['chrome', 'firefox', 'internet explorer', 'phantomjs']
-    },
-    browserVersion: String,
-    sauceLabs: {
-        type: Boolean,
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now
-    },
-    testLastRun: {
-        type: Date,
-    }
-});
-
-var ScreenshotTimeline = mongoose.model('ScreenshotTimeline', screenshotTimelineSchema);
-
-
-var fs = require('fs');
-
+var screenshotController = require('./controllers/screenshot');
 
 router.route('/screenshot')
-    .post(function(req, res, next){
-
-        console.log('post call');
-
-        var query = {screenshotName: req.files.image.originalname};
-
-        Screenshot.findOne(query, function(err, shot){
-            if (err) throw err;
-
-            console.log('Query find the screenshot? ...' + !!shot);
-
-            if (!shot) {
-                console.log('screenshot not in system');
-
-                var newScreenshot = new Screenshot();
-                newScreenshot.screenshotName = req.files.image.originalname;
-                newScreenshot.screenshotImage.image = fs.readFileSync(req.files.image.path);
-                newScreenshot.screenshotImage.encoding = req.files.image.encoding;
-                newScreenshot.screenshotImage.contentType = req.files.image.mimetype;
-
-
-                newScreenshot.save(function(err){
-                    if (err){
-                        console.log(err);
-                        res.end(JSON.stringify(err))
-                    }
-                    console.log('saved screenshot');
-                });
-
-                var query = {testName: newScreenshot.screenshotName}
-                ScreenshotTimeline.findOne(query, function(err, timeline){
-                    if (err) throw err;
-
-                    console.log('Query find the timeline? ...' + !!shot);
-
-                    if (!timeline){
-
-                        console.log('timeline does not exist');
-
-                        var newScreenshotTimeline = new ScreenshotTimeline();
-                        newScreenshotTimeline.testName = newScreenshot.screenshotName;
-                        newScreenshotTimeline.screenshots = [newScreenshot._id];        
-                        newScreenshotTimeline.save(function(err,doc){
-                            if (err){
-                                console.log(err);
-                                res.end(JSON.stringify(err))
-                            }
-                            console.log('saved timeline');
-                        })
-                    } else {
-                        var update = { $addToSet: { screenshots: newScreenshot._id }, $currentDate: { testLastRun: true } }
-                        ScreenshotTimeline.findOneAndUpdate(query, update);
-                        console.log('updated timeline');
-                    }
-                });
-                res.end(JSON.stringify(newScreenshot));
-            } else {
-                console.log('screenshot in system') //+ query.screenshotName)
-                res.send('screenshot in system')//+ query.screenshotName);
-            };
-        });
-    });
+    .post(screenshotController.uploadScreenshot);
 });
 
 
 
 
+var saucelabs = require('./SauceLabs');
+var sauceUsername = 'zhawtof';
+var saucePassword = '502dffd5-0274-4416-b8bf-c623d3be754f';
 
-app.use('/', router)
+var myAccount = new saucelabs({
+    username: sauceUsername,
+    password: saucePassword
+});
+
+var https = require('https');
+
+router.route('/saucelabs')
+    .all(function(req,res,next){
+        myAccount.getServiceStatus(function (err, res) {
+            if (err){
+                res.end('SauceLabs is down');
+            }
+            next();
+        });
+    })
+    .get(function(req, res, next) {
+        myAccount.getJobs(function (err, jobs) {
+        // Get a list of all your jobs
+            for (var k = 0 ; k < (Math.min(1, jobs.length)); k++) {
+                myAccount.showJobAssets(jobs[k].id, function(err, assets) {
+
+                    var str = jobs[k].name + " : ";
+                    for (var m = 0; m < assets.screenshots.length; m++) {
+
+                        str += "\n     " + assets.screenshots[m];
+                        myAccount.getJobAssets(jobs[k].id, assets.screenshots[m], function(err, img){
+
+                            res.json(img);
+
+                        })
+                    }
+                    console.log(str);
+                })
+            }
+        });
+    })
+
+app.use('/', router);
 
 
 /// catch 404 and forward to error handler
@@ -194,4 +119,4 @@ var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 
-module.exports = app;
+module.exports = exports = app;
